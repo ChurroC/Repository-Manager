60efa054786023e744d95c8b80546a5f1b5f0ff9
ChurroC <charanvenkatchandran@gmail.com>
2024-06-16 16:11:45 -0500

    grids the goat

--- vite/src/multipleSortable/MultipleSortable.tsx
+++ vite/src/multipleSortable/MultipleSortable.tsx
@@ -11,7 +11,11 @@ export function MultipleSortable() {
         A: ["A0", "A1", "A2"],
         B: ["B0", "B1"],
         C: ["C0"],
-        D: ["C0"]
+        D: ["D0"],
+        E: ["E0"],
+        F: ["F0"],
+        G: ["G0"],
+        t: ["t0"]
     });
     const previousItems = useRef(items);
     const [columnOrder, setColumnOrder] = useState(
@@ -50,35 +54,26 @@ export function MultipleSortable() {
                 }
             }}
         >
-            <div className="flex justify-around flex-wrap -ml-5 gap-y-5">
+            <div className="grid grid-cols-3 gap-5">
                 {columnOrder.map((columnId, columnIndex) => (
-                    <div className="basis-1/3 pl-5">
-                        <Column
-                            key={columnId}
-                            id={columnId}
-                            index={columnIndex}
-                            className="flex flex-col rounded-md min-w-60 bg-neutral-100 opacity-70 border"
-                        >
-                            <div className="bg-white border-b pl-5 h-11 flex items-center justify-between">
-                                {columnId}
-                                <svg
-                                    viewBox="0 0 20 20"
-                                    width="12"
-                                    className="fill-gray-800"
-                                >
-                                    <path d="M7 2a2 2 0 1 0 .001 4.001A2 2 0 0 0 7 2zm0 6a2 2 0 1 0 .001 4.001A2 2 0 0 0 7 8zm0 6a2 2 0 1 0 .001 4.001A2 2 0 0 0 7 14zm6-8a2 2 0 1 0-.001-4.001A2 2 0 0 0 13 6zm0 2a2 2 0 1 0 .001 4.001A2 2 0 0 0 13 8zm0 6a2 2 0 1 0 .001 4.001A2 2 0 0 0 13 14z"></path>
-                                </svg>
-                            </div>
-                            {items[columnId].map((id, index) => (
-                                <Item
-                                    key={id}
-                                    id={id}
-                                    index={index}
-                                    column={columnId}
-                                />
-                            ))}
-                        </Column>
-                    </div>
+                    <Column
+                        id={columnId}
+                        index={columnIndex}
+                        className="flex flex-col rounded-md min-w-60 bg-neutral-100 opacity-70 border"
+                        key={columnId}
+                    >
+                        <div className="bg-white border-b pl-5 h-11 flex items-center">
+                            {columnId}
+                        </div>
+                        {items[columnId].map((id, index) => (
+                            <Item
+                                key={id}
+                                id={id}
+                                index={index}
+                                column={columnId}
+                            />
+                        ))}
+                    </Column>
                 ))}
             </div>
         </DragDropProvider>
